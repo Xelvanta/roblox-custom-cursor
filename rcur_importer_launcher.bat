2adf9a268658451118fe82a56e98a2606739e1b3
Xelvanta <187991660+Xelvanta@users.noreply.github.com>
2025-05-22 22:12:13 -0700

    Add support for importing .rcur files by opening (double-clicking) them directly.

---
+++ rcur_importer_launcher.bat
@@ -0,0 +1,5 @@
+@echo off
+REM %1 is the file path passed by the shell
+
+REM Call Python with your script and pass the file argument
+pythonw "C:\Program Files\Xelvanta Softworks\rcur_importer.pyw" "%~1"
