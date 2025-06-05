09117eba46c0d58585a91cb2023dc95972a0ba56
Xelvanta <187991660+Xelvanta@users.noreply.github.com>
2025-06-05 04:05:50 -0700

    Update launcher batchfile to reflect in-code value

--- assets/scripts/rcur_importer_launcher.bat
+++ assets/scripts/rcur_importer_launcher.bat
@@ -1,5 +1,5 @@
 @echo off
 REM %1 is the file path passed by the shell
 
-REM Call Python with your script and pass the file argument
-pythonw "C:\Program Files\Xelvanta Softworks\rcur_importer.pyw" "%~1"
+REM Call Python with the script and pass the file argument
+pythonw "%SystemDrive%\Program Files\Xelvanta Softworks\Roblox Custom Cursor\rcur_importer.pyw" "%~1"
